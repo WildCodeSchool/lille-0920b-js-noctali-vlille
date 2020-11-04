941c03d20cb153cdf37d9b9c759decbede8a02c9
AlexisCatrix <alexis.4x.ac@gmail.com>
2020-11-04 13:14:05 +0100

    Add some values for better render of LogoHeader

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,17 +1,23 @@
-import Logo1 from '../images/logo1.png';
-import styled from  'styled-components';
+import Logo1 from "../images/logo1.png";
+import styled from "styled-components";
 
 const HeaderStyled = styled.header`
   display: flex;
+  align-items: center;
   justify-content: left;
   background-color: #242424;
-  height: 10vh;`
+  height: 10vh;
+`;
 
+const LogoHeader = styled.img`
+  height: 6vh;
+  padding-left: 1vw;
+`;
 
 const Header = () => {
   return (
     <HeaderStyled>
-      <img id="logoHeader" src={Logo1}  alt="logo" />
+      <LogoHeader id="logoHeader" src={Logo1} alt="logo" />
     </HeaderStyled>
   );
 };
