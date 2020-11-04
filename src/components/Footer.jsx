9abba99e9a458672e1e701360435f57c95dc46f6
Thomas Druart <thomas.druart.59@gmail.com>
2020-11-04 11:26:50 +0100

    suppression fichiers scss

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,27 +1,27 @@
 import React from "react";
 import logoGps from "../images/logoGps.png";
-import logoLock from "../images/logoLock.png";
-import styled from  'styled-components';
+import logoNote from "../images/logoNote.png";
+import styled from "styled-components";
 
 const FooterStyled = styled.div`
   background-color: #242424;
   width: 100vw;
   height: 10vh;
   display: flex;
-  justify-content: space-around;`
+  justify-content: space-around;
+`;
 
 const ImageStyled = styled.img`
-    height: 90%;
-    width: auto;
-    margin: auto;`
-
-
+  height: 90%;
+  width: auto;
+  margin: auto;
+`;
 
 function Footer() {
   return (
     <FooterStyled>
       <ImageStyled src={logoGps} alt="gps"></ImageStyled>
-      <ImageStyled src={logoLock} alt="note"></ImageStyled>
+      <ImageStyled src={logoNote} alt="note"></ImageStyled>
     </FooterStyled>
   );
 }
