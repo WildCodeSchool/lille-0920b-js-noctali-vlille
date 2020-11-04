7ada78c1a8c0ac23e558d27f02dbdb2fbae3cebc
AlexisCatrix <alexis.4x.ac@gmail.com>
2020-11-04 12:20:31 +0100

    Add a styled-reset for project

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,12 @@ import MapLille from "./components/MapLille";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 import "./App.css";
+import { Reset } from "styled-reset";
 
 function App() {
   return (
     <div>
+      <Reset/>
       <Header />
       <MapLille />
       <Footer />
