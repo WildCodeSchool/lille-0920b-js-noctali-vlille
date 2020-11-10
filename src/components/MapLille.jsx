93169a8c001d1c15716319e3a0f2166e0b11a420
AlexisCatrix <alexis.4x.ac@gmail.com>
2020-11-10 15:18:18 +0100

    Change popup render of station with a color green/red to suit of station's state (work/do not work)

--- src/components/MapLille.jsx
+++ src/components/MapLille.jsx
@@ -1,91 +1,118 @@
 import React from "react";
-
 import axios from "axios";
 import { Map, Marker, TileLayer, Popup } from "react-leaflet";
 import L from "leaflet";
 import { geolocated } from "react-geolocated";
 import icon from "../images/marker-3-4.png";
 import iconShadow from "../images/marker-shadow.png";
+import styled from "styled-components";
+
+const PopupStyled = styled.div`
+  width: 10vw;
+  height: 25vh;
+`;
+
+const IsWorking = styled.li`
+  padding-bottom: 0.75vw;
+  &.work {
+    color: green;
+  }
+  &.dontWork {
+    color: red;
+  }
+`;
 
 delete L.Icon.Default.prototype._getIconUrl;
 
 L.Icon.Default.mergeOptions({
-	iconUrl: icon,
-	shadowUrl: iconShadow,
+  iconUrl: icon,
+  shadowUrl: iconShadow,
 });
 
 class MapLille extends React.Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			stations: [],
-		};
-	}
+  constructor(props) {
+    super(props);
+    this.state = {
+      stations: [],
+    };
+  }
 
-	componentDidMount() {
-		axios
-			.get(
-				"https://opendata.lillemetropole.fr/api/records/1.0/search/?dataset=vlille-realtime&q=&rows=251&facet=libelle&facet=nom&facet=commune&facet=etat&facet=type&facet=etatconnexion"
-			)
-			.then(({ data }) => {
-				this.setState({
-					stations: data.records,
-				});
-			});
-	}
+  componentDidMount() {
+    axios
+      .get(
+        "https://opendata.lillemetropole.fr/api/records/1.0/search/?dataset=vlille-realtime&q=&rows=251&facet=libelle&facet=nom&facet=commune&facet=etat&facet=type&facet=etatconnexion"
+      )
+      .then(({ data }) => {
+        this.setState({
+          stations: data.records,
+        });
+      });
+  }
 
-	render() {
-		const { stations } = this.state;
+  render() {
+    const { stations } = this.state;
 
-		const DEFAULT_LATITUDE = 50.6365654;
-		const DEFAULT_LONGITUDE = 3.0635282;
+    const DEFAULT_LATITUDE = 50.6365654;
+    const DEFAULT_LONGITUDE = 3.0635282;
 
-		const longitude = this.props.coords
-			? this.props.coords.longitude
-			: DEFAULT_LONGITUDE;
+    const longitude = this.props.coords
+      ? this.props.coords.longitude
+      : DEFAULT_LONGITUDE;
 
-		const latitude = this.props.coords
-			? this.props.coords.latitude
-			: DEFAULT_LATITUDE;
+    const latitude = this.props.coords
+      ? this.props.coords.latitude
+      : DEFAULT_LATITUDE;
 
-		return (
-			<div>
-				<Map center={[latitude, longitude]} zoom={14} minZoom={11}>
-					<TileLayer
-						url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-						attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-					/>
+    return (
+      <div>
+        <Map center={[latitude, longitude]} zoom={14} minZoom={11}>
+          <TileLayer
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+          />
 
-					{stations.map((station) => (
-						<Marker
-							key={station.fields.nom}
-							position={[
-								station.geometry.coordinates[1],
-								station.geometry.coordinates[0],
-							]}
-						>
-							<Popup>
-								{station.fields.nom} {station.fields.etat}
-								<br />
-								{station.fields.adresse}
-								<br />
-								{"Vélos dispo "}
-								{station.fields.nbvelosdispo}
-								<br />
-								{"Emplacements dispo "}
-								{station.fields.nbplacesdispo}
-							</Popup>
-						</Marker>
-					))}
-				</Map>
-			</div>
-		);
-	}
+          {stations.map((station) => (
+            <Marker
+              key={station.fields.nom}
+              position={[
+                station.geometry.coordinates[1],
+                station.geometry.coordinates[0],
+              ]}
+            >
+              <Popup>
+                <PopupStyled>
+                  <IsWorking
+                    className={
+                      station.fields.etat.includes("HORS SERVICE")
+                        ? "dontWork"
+                        : "work"
+                    }
+                  >
+                    {station.fields.etat}
+                  </IsWorking>
+                  {station.fields.nom}
+                  <br />
+                  {station.fields.adresse}
+                  <br />
+
+                  {"Vélos dispo "}
+                  {station.fields.nbvelosdispo}
+                  <br />
+                  {"Emplacements dispo "}
+                  {station.fields.nbplacesdispo}
+                </PopupStyled>
+              </Popup>
+            </Marker>
+          ))}
+        </Map>
+      </div>
+    );
+  }
 }
 
 export default geolocated({
-	positionOptions: {
-		enableHighAccuracy: false,
-	},
-	userDecisionTimeout: 10000,
+  positionOptions: {
+    enableHighAccuracy: false,
+  },
+  userDecisionTimeout: 10000,
 })(MapLille);
