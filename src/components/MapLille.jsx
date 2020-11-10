e06879ed9f940a15ca6a60a764966384a735303e
Thomas Druart <thomas.druart.59@gmail.com>
2020-11-10 10:35:54 +0100

    start working on conditional markers

--- src/components/MapLille.jsx
+++ src/components/MapLille.jsx
@@ -1,7 +1,18 @@
 import React from "react";
+
 import axios from "axios";
 import { Map, Marker, TileLayer, Popup } from "react-leaflet";
+import L from "leaflet";
 import { geolocated } from "react-geolocated";
+import icon from "../images/marker-3-4.png";
+import iconShadow from "../images/marker-shadow.png";
+
+delete L.Icon.Default.prototype._getIconUrl;
+
+L.Icon.Default.mergeOptions({
+  iconUrl: icon,
+  shadowUrl: iconShadow,
+});
 
 class MapLille extends React.Component {
   constructor(props) {
@@ -16,7 +27,7 @@ class MapLille extends React.Component {
       .get(
         "https://opendata.lillemetropole.fr/api/records/1.0/search/?dataset=vlille-realtime&q=&rows=251&facet=libelle&facet=nom&facet=commune&facet=etat&facet=type&facet=etatconnexion"
       )
-      .then(({data}) => {
+      .then(({ data }) => {
         this.setState({
           stations: data.records,
         });
@@ -39,7 +50,7 @@ class MapLille extends React.Component {
 
     return (
       <div>
-        <Map center={[latitude, longitude]} zoom={14}>
+        <Map center={[latitude, longitude]} zoom={14} minZoom={11}>
           <TileLayer
             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
             attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
@@ -54,9 +65,15 @@ class MapLille extends React.Component {
               ]}
             >
               <Popup>
-                {station.fields.nom}
+                {station.fields.nom} {station.fields.etat}
                 <br />
                 {station.fields.adresse}
+                <br />
+                {"Vélos dispo "}
+                {station.fields.nbvelosdispo}
+                <br />
+                {"Emplacements dispo "}
+                {station.fields.nbplacesdispo}
               </Popup>
             </Marker>
           ))}
