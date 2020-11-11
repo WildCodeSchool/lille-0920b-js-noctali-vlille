587450957b099f2b4c08fc92d13ac905f34f26f6
gaetan-cordonnier <gaetan.cordonnier@gmail.com>
2020-11-11 12:37:14 +0100

    Component stationList created with TPE and Online displayed. Logos stationlist and reminder modified.

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,17 @@ import Footer from "./components/Footer";
 import Header from "./components/Header";
 import "./App.css";
 import { Reset } from "styled-reset";
+import StationsList from "./components/StationsList";
 
 function App() {
-  return (
-    <div>
-      <Reset />
-      <Header />
-      <MapLille />
-      <Footer />
-    </div>
-  );
+	return (
+		<div>
+			<Reset />
+			<Header />
+			<StationsList />
+			<Footer />
+		</div>
+	);
 }
 
 export default App;
