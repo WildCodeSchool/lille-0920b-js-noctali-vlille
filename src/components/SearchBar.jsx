3b5383e28094b13d5c9c13bdeb182f5afe479744
AlexisCatrix <alexis.4x.ac@gmail.com>
2020-11-17 22:01:34 +0100

    #1 Create a SearchBar component to make searches of sations in Lille(done). Second step => transform searching with a sort on first char

---
+++ src/components/SearchBar.jsx
@@ -0,0 +1,72 @@
+import { useState } from "react";
+import axios from "axios";
+import styled from "styled-components";
+
+const InputStyled = styled.input`
+  width: 20vw;
+  margin: 0vh 2vw;
+`;
+
+const SumbitSearch = styled.button`
+  width: 8vw;
+  height: 4vh;
+`;
+
+const SelectContainer = styled.ul`
+  position: absolute;
+  z-index: 1;
+`;
+
+const SelectStation = styled.li`
+  display: flex;
+  align-items: center;
+  width: 20vw;
+  height: 5vh;
+  border: 1px solid #e26262;
+  background-color: #461d1d;
+  list-style: none;
+  margin-left: 2vw;
+  padding-left: 1vw;
+  /* height: 10vh; */
+  color: #fed96a;
+`;
+
+export default function SearchBar() {
+  const [commune, setCommune] = useState([]);
+
+  const findSearch = (stationsCity) => {
+    axios
+      .get(
+        `https://opendata.lillemetropole.fr/api/records/1.0/search/?dataset=vlille-realtime&q=&rows=25&facet=libelle&facet=nom&facet=commune&facet=etat&facet=type&facet=etatconnexion`
+      )
+      .then(({ data }) => {
+        let { records } = data;
+
+        records = records.filter((record) => {
+          return record.fields.commune.includes(stationsCity.toUpperCase());
+          // la methode toUpperCase permet de récupérer la valeur de "commune"; qui est une chaine de caractère
+          // en majuscule. Ainsi la recherche fonctionne en minuscule et en majuscule dans l'input.
+        });
+        setCommune(records);
+        console.log(records);
+      })
+      .catch((err) => {
+        console.log(err);
+        console.log("It is a failure");
+      });
+  };
+
+  return (
+    <div>
+      <InputStyled type="text" onChange={(e) => findSearch(e.target.value)} />
+      <SumbitSearch type="submit">Send</SumbitSearch>
+      <SelectContainer>
+        {commune.map((station) => (
+          <SelectStation>
+            {station.fields.commune} {station.fields.nom}
+          </SelectStation>
+        ))}
+      </SelectContainer>
+    </div>
+  );
+}
