a2c99c98294c3acb47770265080f395922eb620a
AlexisCatrix <alexis.4x.ac@gmail.com>
2020-11-18 20:45:39 +0100

    Filing of the stylization code in a separate file for a better visibility of the functional code.

---
+++ src/ComponentsStyled/SearchBarStyled.js
@@ -0,0 +1,39 @@
+import styled from "styled-components";
+
+
+export const InputStyled = styled.input`
+width: 30vw;
+margin: 0vh 2vw;
+`;
+
+export const SumbitSearch = styled.button`
+width: 8vw;
+height: 4vh;
+`;
+
+export const SelectContainer = styled.ul`
+position: absolute;
+z-index: 1;
+`;
+
+export const SelectStation = styled.li`
+display: flex;
+align-items: center;
+width: 30vw;
+height: 5vh;
+border: 1px solid #e26262;
+background-color: #461d1d;
+list-style: none;
+margin-left: 2vw;
+padding-left: 1vw;
+color: #fed96a;
+`;
+
+export const CityStations = styled.li`
+font-style: italic;
+`;
+
+export const NameStations = styled.li`
+font-weight: bold;
+margin-left: 0.5vw;
+`;
