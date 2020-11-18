31104a60fd6d32a9bea425aec26c1bde0efa4c1e
gaetan-cordonnier <gaetan.cordonnier@gmail.com>
2020-11-18 10:18:37 +0100

    Add component Reminder

--- src/App.jsx
+++ src/App.jsx
@@ -8,34 +8,38 @@ import About from "./components/About";
 import AppDownload from "./components/AppDownload";
 import TeamNoctali from "./components/TeamNoctali";
 import StationsList from "./components/StationsList";
+import Reminder from "./components/Reminder";
 
 function App() {
-  return (
-    <div>
-      <Reset />
+	return (
+		<div>
+			<Reset />
 
-      <Header />
+			<Header />
 
-      <Switch>
-        <Route exact path="/">
-          <MapLille />
-        </Route>
-        <Route path="/about">
-          <About />
-        </Route>
-        <Route path="/appdownload">
-          <AppDownload />
-        </Route>
-        <Route path="/teamnoctali">
-          <TeamNoctali />
-        </Route>
-        <Route path="/stationslist">
-          <StationsList />
-        </Route>
-      </Switch>
-      <Footer />
-    </div>
-  );
+			<Switch>
+				<Route exact path="/">
+					<MapLille />
+				</Route>
+				<Route path="/about">
+					<About />
+				</Route>
+				<Route path="/appdownload">
+					<AppDownload />
+				</Route>
+				<Route path="/teamnoctali">
+					<TeamNoctali />
+				</Route>
+				<Route path="/stationslist">
+					<StationsList />
+				</Route>
+				<Route path="/reminder">
+					<Reminder />
+				</Route>
+			</Switch>
+			<Footer />
+		</div>
+	);
 }
 
 export default App;
