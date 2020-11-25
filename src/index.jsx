6af570b9e1dfbc1c811fa23ed3ccec5d53f304d5
AlexisCatrix <alexis.4x.ac@gmail.com>
2020-11-25 18:49:00 +0100

    Add react-router-dom

--- src/index.jsx
+++ src/index.jsx
@@ -1,10 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { BrowserRouter } from "react-router-dom";
 import App from "./App";
+import { Reset } from "styled-reset";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Reset />
+      <App />
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById("root")
 );
