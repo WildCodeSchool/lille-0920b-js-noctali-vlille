d9c74fb1b80942623d177281fa0082d9f195ef74
gaetan-cordonnier <gaetan.cordonnier@gmail.com>
2020-11-27 14:48:13 +0100

    reminder size styled modified

--- src/components/Reminder.jsx
+++ src/components/Reminder.jsx
@@ -6,121 +6,124 @@ import { CloseCircle } from "@styled-icons/ionicons-sharp/CloseCircle";
 import { Link } from "react-router-dom";
 
 const Background = styled.div`
-  display: flex;
-  background-color: transparent;
-  height: 80vh;
-  background-color: lightgrey;
+	display: flex;
+	background-color: transparent;
+	height: 80vh;
+	background-color: lightgrey;
 `;
 
 const ReminderStyled = styled.div`
-  width: 80vw;
-  height: 35vh;
-  display: flex;
-  margin: auto;
-  flex-direction: column;
-  align-items: center;
-  justify-content: space-between;
-  display: flex;
-  font-family: "Montserrat", sans-serif;
-  background-color: #242424;
-  border-radius: 1.5rem;
-  border: solid 3px #fed45e;
+	width: 80vw;
+	height: 35vh;
+	display: flex;
+	margin: auto;
+	flex-direction: column;
+	align-items: center;
+	justify-content: space-between;
+	display: flex;
+	font-family: "Montserrat", sans-serif;
+	background-color: #242424;
+	border-radius: 1.5rem;
+	border: solid 3px #fed45e;
+	@media screen and (min-width: 480px) {
+		width: 20vw;
+	}
 `;
 
 const IconCloseStyle = styled.div`
-  display: flex;
-  flex-direction: row;
-  width: 100%;
-  justify-content: flex-end;
+	display: flex;
+	flex-direction: row;
+	width: 100%;
+	justify-content: flex-end;
 `;
 
 const IconClose = styled(CloseCircle)`
-  margin-right: 10px;
-  margin-top: 10px;
-  height: 2rem;
-  width: auto;
-  color: #fff;
+	margin-right: 10px;
+	margin-top: 10px;
+	height: 2rem;
+	width: auto;
+	color: #fff;
 `;
 
 const IconZap = styled(Lock)`
-  height: 2rem;
-  width: auto;
-  color: #fed45e;
-  margin-bottom: 20px;
+	height: 2rem;
+	width: auto;
+	color: #fed45e;
+	margin-bottom: 20px;
 `;
 
 const TitleStyled = styled.h1`
-  font-size: 1.2rem;
-  color: #fff;
-  text-align: center;
-  margin-bottom: 20px;
+	font-size: 1.2rem;
+	color: #fff;
+	text-align: center;
+	margin-bottom: 20px;
 `;
 
 const InputStyled = styled.input`
-  width: 30%;
-  height: 2rem;
-  background: #242424;
-  font-size: 1.22rem;
-  color: #fed45e;
-  border-bottom: 2px solid #fed45e;
-  border-radius: 3px;
-  text-align: center;
-  border-left: none;
-  border-top: none;
-  border-right: none;
-  margin-bottom: 20px;
+	width: 3rem;
+	height: 2rem;
+	background: #242424;
+	font-size: 1.22rem;
+	color: #fed45e;
+	border-bottom: 2px solid #fed45e;
+	border-radius: 3px;
+	text-align: center;
+	border-left: none;
+	border-top: none;
+	border-right: none;
+	margin-bottom: 20px;
 `;
 
 const ButtonStyled = styled.button`
-  border: none;
-  padding: 6px 6px 6px 6px;
-  border-radius: 10px;
-  background: #fed45e;
-  font-weight: bold 13px;
-  color: #242424;
-  vertical-align: middle;
-  outline: none;
-  margin-bottom: 20px;
+	border: none;
+	padding: 6px 6px 6px 6px;
+	border-radius: 10px;
+	background: #fed45e;
+	font-weight: bold 13px;
+	color: #242424;
+	vertical-align: middle;
+	outline: none;
+	margin-bottom: 20px;
 `;
 
 const useStateCode = (localStorageKey) => {
-  const [value, setValue] = useState(
-    localStorage.getItem(localStorageKey) || ""
-  );
+	const [value, setValue] = useState(
+		localStorage.getItem(localStorageKey) || ""
+	);
 
-  useEffect(() => {
-    localStorage.setItem(localStorageKey, value);
-  }, [localStorageKey, value]);
+	useEffect(() => {
+		localStorage.setItem(localStorageKey, value);
+	}, [localStorageKey, value]);
 
-  return [value, setValue];
+	return [value, setValue];
 };
 
 const Reminder = () => {
-  const [value, setValue] = useStateCode("Code :");
+	const [value, setValue] = useStateCode("Code :");
 
-  const onChange = (event) => setValue(event.target.value);
+	const onChange = (event) => setValue(event.target.value);
 
-  return (
-    <Background>
-      <ReminderStyled>
-        <IconCloseStyle>
-          <Link to="/">
-            <IconClose />
-          </Link>
-        </IconCloseStyle>
-        <IconZap />
-        <TitleStyled>Code de vérouillage :</TitleStyled>
-        <InputStyled
-          maxLength="4"
-          value={value}
-          type="text"
-          id="Code"
-          onChange={onChange}
-        />
-        <ButtonStyled type="submit">Sauvegarder</ButtonStyled>
-      </ReminderStyled>
-    </Background>
-  );
+	return (
+		<Background>
+			<ReminderStyled>
+				<IconCloseStyle>
+					<Link to="/">
+						<IconClose />
+					</Link>
+				</IconCloseStyle>
+				<IconZap />
+				<TitleStyled>Code de vérouillage :</TitleStyled>
+				<InputStyled
+					maxLength="4"
+					value={value}
+					type="text"
+					id="Code"
+					onChange={onChange}
+				/>
+				<ButtonStyled type="submit">Sauvegarder</ButtonStyled>
+			</ReminderStyled>
+		</Background>
+	);
 };
 
 export default Reminder;
