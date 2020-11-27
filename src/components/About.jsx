5d5a5f738eb211626eb12e317301d2fb6c249324
gaetan-cordonnier <gaetan.cordonnier@gmail.com>
2020-11-27 10:40:57 +0100

    Fonts and color adjusted

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -4,117 +4,120 @@ import styled from "styled-components";
 import { Link } from "react-router-dom";
 
 const DivStyled = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  width: auto;
-  height: 80vh;
-  background-color: #242424;
-  @media (max-width: 768px) {
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-  }
+	font-family: "Montserrat", sans-serif;
+	font-size: 1.2em;
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	width: auto;
+	height: 80vh;
+	background-color: #242424;
+	@media (max-width: 768px) {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		font-size: 0.9em;
+	}
 `;
 
 const ImgStyled = styled.img`
-  width: 300px;
-  height: auto;
-  display: block;
-  margin: 2vh;
-  @media (max-width: 768px) {
-    width: auto;
-    height: 30vh;
-  }
+	width: 300px;
+	height: auto;
+	display: block;
+	margin: 2vh;
+	@media (max-width: 768px) {
+		width: auto;
+		height: 30vh;
+	}
 `;
 
 const Title = styled.h1`
-  font-family: "Roboto", Arial, sans-serif;
-  font-size: 30px;
-  color: orange;
-  @media (max-width: 768px) {
-    display: flex;
-    justify-content: center;
-    margin: 20px;
-  }
+	font-family: "Roboto", Arial, sans-serif;
+	font-size: 30px;
+	color: #fed45e;
+	@media (max-width: 768px) {
+		display: flex;
+		justify-content: center;
+		margin: 20px;
+	}
 `;
 
 const Paragraphe = styled.div`
-  color: white;
-  margin: 2vh;
+	color: white;
+	margin: 2vh;
 `;
 
 const LinkStyled = styled(Link)`
-  text-decoration: none;
+	text-decoration: none;
 `;
 
 const Span = styled.span`
-  color: orange;
-  @media (max-width: 768px) {
-    display: flex;
-    justify-content: center;
-  }
+	color: #fed45e;
+	@media (max-width: 768px) {
+		display: flex;
+		justify-content: center;
+	}
 `;
 
 const Vlille = styled.a`
-  float: right;
-  text-decoration: none;
-  color: orange;
-  @media (max-width: 768px) {
-    display: none;
-  }
+	float: right;
+	text-decoration: none;
+	color: #fed45e;
+	@media (max-width: 768px) {
+		display: none;
+	}
 `;
 
 const About = () => {
-  return (
-    <DivStyled>
-      <Title>Doukilemin V'lille</Title>
+	return (
+		<DivStyled>
+			<Title>Doukilemin V'lille</Title>
 
-      <ImgStyled id="Velo1" src={Velo1} alt="logovelo" />
-      <Paragraphe>
-        <p>
-          Doukilemin V'lille est dédié aux utilisateurs du V'lille et de son
-          service VLS (Vélos en Libre Service).
-        </p>
-        <p>
-          Au quotidien, l'application vous permet de gérer facilement:
-          <ul>
-            <br />
-            <li>
-              {" "}
-              - vos stations favorites afin de contrôler le nombre de vélos ou
-              emplacements disponibles.
-            </li>
-            <br />
-            <li>
-              {" "}
-              - trouver une station proche de votre position grace à la
-              localisation.
-            </li>
-          </ul>
-        </p>
-        <p>
-          <br />
-          Dès l'écran d'accueil, Doukilemin V'lille affiche l'état des stations,
-          évitant ainsi les menus superflus.
-        </p>
-        <ul>
-          <li>
-            <br />
-            <LinkStyled to="/appdownload">
-              <Span>Télécharger l'application</Span>
-            </LinkStyled>
-            <Vlille
-              href="https://www.goodmorninglille.org/blog/vlille-lille-location-de-velos"
-              target="_blank"
-            >
-              Guide d'utilisation V'lille
-            </Vlille>
-          </li>
-        </ul>
-      </Paragraphe>
-    </DivStyled>
-  );
+			<ImgStyled id="Velo1" src={Velo1} alt="logovelo" />
+			<Paragraphe>
+				<p>
+					Doukilemin V'lille est dédié aux utilisateurs du V'lille et de son
+					service VLS (Vélos en Libre Service).
+				</p>
+				<p>
+					Au quotidien, l'application vous permet de gérer facilement:
+					<ul>
+						<br />
+						<li>
+							{" "}
+							- vos stations favorites afin de contrôler le nombre de vélos ou
+							emplacements disponibles.
+						</li>
+						<br />
+						<li>
+							{" "}
+							- trouver une station proche de votre position grace à la
+							localisation.
+						</li>
+					</ul>
+				</p>
+				<p>
+					<br />
+					Dès l'écran d'accueil, Doukilemin V'lille affiche l'état des stations,
+					évitant ainsi les menus superflus.
+				</p>
+				<ul>
+					<li>
+						<br />
+						<LinkStyled to="/appdownload">
+							<Span>Télécharger l'application</Span>
+						</LinkStyled>
+						<Vlille
+							href="https://www.goodmorninglille.org/blog/vlille-lille-location-de-velos"
+							target="_blank"
+						>
+							Guide d'utilisation V'lille
+						</Vlille>
+					</li>
+				</ul>
+			</Paragraphe>
+		</DivStyled>
+	);
 };
 
 export default About;
